96443b66bd67f264798d95abdf1e6b147d1b6721
Augusto Vilela <avilelateixeira@gmail.com>
2017-08-08 22:48:22 +0000

    Devise views

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,15 @@ module Hackthon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.i18n.default_locale = :'pt-BR'
+    config.generators do |g|
+  		g.javascripts = false
+  		g.helper = false
+  		g.view_specs = false
+  		g.helper_specs = false
+  		g.controller_specs = false
+  		g.model_specs = false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
