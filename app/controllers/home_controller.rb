621beeddf1c1161dc538a9e055fbcb29cdee3fca
Augusto Vilela <avilelateixeira@gmail.com>
2017-08-10 20:35:45 +0000

    home create occurence

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,13 @@ class HomeController < ApplicationController
 		@occurrence = new Occurrence(occurrence)
 		@occurrence.save
 	end
+
+
+	private
+		def occurrence_params
+	      params.require(:occurrence).permit(
+	      	:data_begin, :status, :description, :user_id, :date_end, 
+	      	:photo_occurrences_attributes =>[:id, :photo, :_destroy]
+      		)
+	    end
 end
