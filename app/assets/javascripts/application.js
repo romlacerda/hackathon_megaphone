f37c011f9c13c99189ab4f443a49ab99161b6b27
Augusto Vilela <avilelateixeira@gmail.com>
2017-08-12 17:24:28 +0000

    ms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -166,3 +166,17 @@ function limpaModal() {
 	$("#occurrence_status").val('');
 	$("#occurrence_type_id").val('');
 }
+
+
+$('#new_occurrence').submit(function() {  
+    var valuesToSubmit = $(this).serialize();
+    $.ajax({
+        type: "POST",
+        url: $(this).attr('action'), //sumbits it to the given url of the form
+        data: valuesToSubmit,
+        dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
+    }).success(function(json){
+        console.log("success", json);
+    });
+    return false; // prevents normal behaviour
+});
