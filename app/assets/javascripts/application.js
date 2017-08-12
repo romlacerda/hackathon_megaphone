41528a207e703a4c12cf574f9358e753e738fa9a
Augusto Vilela <avilelateixeira@gmail.com>
2017-08-12 17:31:38 +0000

    teste

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -168,15 +168,17 @@ function limpaModal() {
 }
 
 
-$('#new_occurrence').submit(function() {  
-    var valuesToSubmit = $(this).serialize();
-    $.ajax({
-        type: "POST",
-        url: $(this).attr('action'), //sumbits it to the given url of the form
-        data: valuesToSubmit,
-        dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-    }).success(function(json){
-        console.log("success", json);
-    });
-    return false; // prevents normal behaviour
+$(document).ready(function(){
+	$('#new_occurrence').submit(function() {  
+	    var valuesToSubmit = $(this).serialize();
+	    $.ajax({
+	        type: "POST",
+	        url: $(this).attr('action'), //sumbits it to the given url of the form
+	        data: valuesToSubmit,
+	        dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
+	    }).success(function(json){
+	        console.log("success", json);
+	    });
+	    return false; // prevents normal behaviour
+	});
 });
